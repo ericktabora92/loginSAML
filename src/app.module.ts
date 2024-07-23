78a0f0de0c52be6e2715a38a1d4fd7a4456d7a91
Erick Gerardo Tábora Rivera <ericktabora92@gmail.com>
2024-07-22 20:08:33 -0600

    feat: enviroments variables and constants

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { ModelModule } from './model/model.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    ModelModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/constants.ts
@@ -0,0 +1,4 @@
+export const SAML_PROFILE = {
+  username: 'http://schemas.auth0.com/nickname',
+  email: 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress',
+};
