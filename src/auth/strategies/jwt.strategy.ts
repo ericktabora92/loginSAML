dc13c2cecd3423517ebd7c0a845dd9a5886e9a0f
Erick Gerardo Tábora Rivera <ericktabora92@gmail.com>
2024-07-22 20:07:04 -0600

    feat: jwt implementation

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { User } from 'src/model/user';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    const user: User | undefined = this.userService.retrieveUser(payload.sub);
+    if (user) {
+      return user;
+    }
+
+    throw new ForbiddenException('user was not found');
+  }
+}
